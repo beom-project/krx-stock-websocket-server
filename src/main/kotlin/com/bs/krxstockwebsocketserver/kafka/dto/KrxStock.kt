2c96667bf7ca793db1c2392c8a95e5200a72662c
Park Beomsun <beomsun22@gmail.com>
2023-10-20 10:38:44 +0900

    fix: 문법 오류 수정

--- src/main/kotlin/com/bs/krxstockwebsocketserver/kafka/dto/KrxStock.kt
+++ src/main/kotlin/com/bs/krxstockwebsocketserver/kafka/dto/KrxStock.kt
@@ -15,7 +15,7 @@ data class KrxStock(
     val marketCap:String,
 ){
     fun setFluctuationSign(){
-        if (this.fluctuationRange.toInt() = 0) return
+        if (this.fluctuationRange.toInt() == 0) return
         if (this.fluctuationRange.toInt() > 0 ) {
             this.fluctuationSign = "+"
             return
